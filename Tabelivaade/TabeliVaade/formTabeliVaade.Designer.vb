7e7bc412ee4e956bf8e11a68725b47035035783e
RamsesKlap <rasmus.palk@gmail.com>
2025-03-27 19:38:43 +0200

    Lisasin autode probleemi klassi ja liidese ning kasutajaliidesele lisasin probleemide vaate

--- Tabelivaade/TabeliVaade/formTabeliVaade.Designer.vb
+++ Tabelivaade/TabeliVaade/formTabeliVaade.Designer.vb
@@ -24,33 +24,49 @@ Partial Class formTabeliVaade
     Private Sub InitializeComponent()
         Me.dgvTabeliVaade = New System.Windows.Forms.DataGridView()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnProbleemid = New System.Windows.Forms.Button()
         CType(Me.dgvTabeliVaade, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgvTabeliVaade
         '
         Me.dgvTabeliVaade.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvTabeliVaade.Location = New System.Drawing.Point(12, 12)
+        Me.dgvTabeliVaade.Location = New System.Drawing.Point(16, 15)
+        Me.dgvTabeliVaade.Margin = New System.Windows.Forms.Padding(4)
         Me.dgvTabeliVaade.Name = "dgvTabeliVaade"
-        Me.dgvTabeliVaade.Size = New System.Drawing.Size(776, 385)
+        Me.dgvTabeliVaade.RowHeadersWidth = 51
+        Me.dgvTabeliVaade.Size = New System.Drawing.Size(1035, 474)
         Me.dgvTabeliVaade.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(373, 404)
+        Me.Label1.Location = New System.Drawing.Point(497, 497)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.Size = New System.Drawing.Size(43, 16)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Tabel"
         '
+        'btnProbleemid
+        '
+        Me.btnProbleemid.Location = New System.Drawing.Point(16, 510)
+        Me.btnProbleemid.Name = "btnProbleemid"
+        Me.btnProbleemid.Size = New System.Drawing.Size(99, 23)
+        Me.btnProbleemid.TabIndex = 2
+        Me.btnProbleemid.Text = "Probleemid"
+        Me.btnProbleemid.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnProbleemid.UseVisualStyleBackColor = True
+        '
         'formTabeliVaade
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1067, 554)
+        Me.Controls.Add(Me.btnProbleemid)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.dgvTabeliVaade)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "formTabeliVaade"
         Me.Text = "Form1"
         CType(Me.dgvTabeliVaade, System.ComponentModel.ISupportInitialize).EndInit()
@@ -61,4 +77,5 @@ Partial Class formTabeliVaade
 
     Friend WithEvents dgvTabeliVaade As DataGridView
     Friend WithEvents Label1 As Label
+    Friend WithEvents btnProbleemid As Button
 End Class
--- Tabelivaade/TabeliVaade/formTabeliVaade.vb
+++ Tabelivaade/TabeliVaade/formTabeliVaade.vb
@@ -2,6 +2,7 @@
 
 Public Class formTabeliVaade
     Private autodList As New BindingList(Of CAuto)()
+    Private probleemidList As New BindingList(Of CAutoProbleem)()
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Manuaalne autode lisamine, asendme hiljem JSON faili lugemisega
@@ -9,6 +10,20 @@ Public Class formTabeliVaade
         autodList.Add(New CAuto(1, "Honda Civic", 15003.2, 6.8, False, False))
         autodList.Add(New CAuto(1, "Ford Focus", 9999, 7.5, True, False))
 
+        probleemidList.Add(New CAutoProbleem(0, "Mootor väriseb", True, False))
+        probleemidList.Add(New CAutoProbleem(2, "Mootor puudub", False, True))
+
         dgvTabeliVaade.DataSource = autodList
+    End Sub
+
+    Private Sub btnProbleemid_Click(sender As Object, e As EventArgs) Handles btnProbleemid.Click
+        If btnProbleemid.Text Is "Probleemid" Then
+            btnProbleemid.Text = "Autod"
+
+            dgvTabeliVaade.DataSource = probleemidList
+        Else
+            dgvTabeliVaade.DataSource = autodList
+            btnProbleemid.Text = "Probleemid"
+        End If
     End Sub
 End Class
