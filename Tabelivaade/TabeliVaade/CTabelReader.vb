84d7a689fa6a84ed9f76fa053bae1e550af1a980
zunzix <michalkarl@gmail.com>
2025-03-30 17:50:50 +0300

    CTabelReader loodud, et tabelit oleks kuskilt kätte saada.

---
+++ Tabelivaade/TabeliVaade/CTabelReader.vb
@@ -0,0 +1,20 @@
+﻿Imports System.ComponentModel
+
+Public Class CTabelReader
+
+    Private Shared _instance As New CTabelReader()
+
+    Public autodList As New BindingList(Of CAuto)()
+
+    ' siin toimub autode lisamine tabelisse
+    Private Sub New()
+        autodList.Add(New CAuto(1, "Peugeot 206", 178989, 11.2, True, False))
+        autodList.Add(New CAuto(2, "Honda Civic", 15003.2, 6.8, False, False))
+        autodList.Add(New CAuto(3, "Ford Focus", 9999, 7.5, True, False))
+    End Sub
+
+    Public Shared Function GetInstance() As CTabelReader
+        Return _instance
+    End Function
+
+End Class
