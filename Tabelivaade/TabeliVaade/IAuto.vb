5e09b15f927728d4c911fd9f7ffb6158c6da68a1
RamsesKlap <rasmus.palk@gmail.com>
2025-04-01 09:14:21 +0300

    Lisasin puuduvad failid

---
+++ Tabelivaade/TabeliVaade/IAuto.vb
@@ -0,0 +1,9 @@
+﻿Public Interface IAuto
+    Property ID As Integer
+    Property GroupID As Integer
+    Property Name As String
+    Property Mileage As Double
+    Property avgFuelConsumption As Double
+    Property IsAvailable As Boolean
+    Property IsArchived As Boolean
+End Interface
---
+++ Tabelivaade/TabeliVaade/IAutoProbleem.vb
@@ -0,0 +1,7 @@
+﻿Public Interface IAutoProbleem
+    Property ID As Integer
+    Property CarID As Integer
+    Property Issue As String
+    Property IsCritical As Boolean
+    Property IsResolved As Boolean
+End Interface
