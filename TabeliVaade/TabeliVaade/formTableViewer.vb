c559f6ed6c745a988c7c8f907c49034484080207
RamsesKlap <rasmus.palk@gmail.com>
2025-04-21 12:17:12 +0300

    Koondasin funktsioone formsis ning kommenteerisin olemasolevaid funktsioone

--- TabeliVaade/TabeliVaade/formTableViewer.vb
+++ TabeliVaade/TabeliVaade/formTableViewer.vb
@@ -16,24 +16,51 @@ Public Class formTableViewer
 
         ' TODO: Bringing in lists and view them on DataGridView
 
-        For Each row As DataGridViewRow In dgvCarsList.Rows
-            If Not row.IsNewRow Then
-                If row.Cells("IsInCriticalState").Value Then
-                    row.Cells("IsInCriticalState").Style.BackColor = Color.Red
-                End If
-            End If
-        Next
+        'For Each row As DataGridViewRow In dgvCarsList.Rows
+        '    If Not row.IsNewRow Then
+        '        If row.Cells("IsInCriticalState").Value Then
+        '            row.Cells("IsInCriticalState").Style.BackColor = Color.Coral
+        '        End If
+        '    End If
+        'Next
 
-        ' Add a remove ( - ) button after every column
+        ' Add fields for the car model, whether it needs maintenence and whether it's available and the delete button
+        Dim carModel As New DataGridViewTextBoxColumn()
+        carModel.Name = "CarModel"
+        carModel.HeaderText = "Car Model"
+        carModel.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        dgvCarsList.Columns.Add(carModel)
+
+        Dim maintenence As New DataGridViewCheckBoxColumn()
+        maintenence.Name = "Maintenence"
+        maintenence.HeaderText = "Maintenence"
+        maintenence.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+        dgvCarsList.Columns.Add(maintenence)
+
+        Dim availability As New DataGridViewCheckBoxColumn()
+        availability.Name = "isAvailable"
+        availability.HeaderText = "Availability"
+        availability.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+        dgvCarsList.Columns.Add(availability)
+
         Dim subtractButton As New DataGridViewButtonColumn()
         subtractButton.Name = "DeleteButton"
         subtractButton.HeaderText = ""
-        subtractButton.Text = "-"
+        subtractButton.Text = "Delete"
         subtractButton.UseColumnTextForButtonValue = True
+        subtractButton.AutoSizeMode = DataGridViewAutoSizeColumnMode.None
+        subtractButton.Width = 50
         dgvCarsList.Columns.Add(subtractButton)
 
-        ' Set the AutoSizeColumnsMode to AllCells
-        dgvCarsList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnMode.AllCells
+        ' Set the DataGridView to ReadOnly so that you can't change the fields right on the table
+        dgvCarsList.ReadOnly = True
+
+        ' Remove the annoying row headers and the beginning of the list
+        dgvCarsList.RowHeadersVisible = False
+
+        ' Change the select color from an eye piercing blue to a more subtle gray
+        dgvCarsList.DefaultCellStyle.SelectionBackColor = Color.LightGray
+
     End Sub
 
     ' Description:  Checks whether input fields for adding a car are valid or not
@@ -70,6 +97,10 @@ Public Class formTableViewer
         Return True
     End Function
 
+
+    ' Description:  Clears the flieds when adding a new car
+    ' Parameters:   The usual handler parameters
+    ' Return:       NONE
     Private Sub txtAddCarFields_Click(sender As Object, e As EventArgs) _
         Handles txtAvgFuel.Click, txtMileage.Click, txtName.Click
         ' Figure out which textbox was clicked
@@ -83,6 +114,9 @@ Public Class formTableViewer
         GC.WaitForPendingFinalizers()
     End Sub
 
+    ' Description:  Checks whether the delete button of the row has been pressed and removes it
+    ' Parameters:   Default handler parameters
+    ' Return:       NONE
     Private Sub dgvCarsList_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvCarsList.CellContentClick
         ' Check if the clicked cell is in the header or not
         If e.RowIndex < 0 Then
@@ -96,30 +130,34 @@ Public Class formTableViewer
         End If
     End Sub
 
+    ' Description:  Double clicking one of the fields changes tabs to the problems view
+    ' Parameters:   Default handler parameters
+    ' Return:       NONE
     Private Sub dgvCarsList_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvCarsList.CellDoubleClick
         ' Check if the clicked cell is in the header or not
         If e.RowIndex < 0 Then
             Return
         End If
 
-        ' Assuming you want to get the value from a specific column (e.g., "AvgFuelConsumption") of the selected row
-        lblFuelData.Text = carsList(e.RowIndex).AvgFuelConsumption.ToString()
-        lblAvailabilityData.Text = carsList(e.RowIndex).IsAvailable.ToString()
-        lblMilageData.Text = carsList(e.RowIndex).Mileage.ToString()
-        ' Use LINQ to find the specific insurance item with the matching CarID
-        Dim selectedCarID As Integer = CInt(dgvCarsList.Rows(e.RowIndex).Cells("ID").Value)
-        Dim insurance As CInsurance = insuranceList.FirstOrDefault(Function(c) c.CarID = selectedCarID)
+        ' Assign detailed values to the data fields
+        ' TODO: Make it so that you get the data from the database
+        lblFuelData.Text = "-1"
+        lblAvailabilityData.Text = "-1"
+        lblMilageData.Text = "-1"
+
+        ' Find whether the selected car has any ongoing insurances
+        ' TODO: Make it possible to search through the database for insurance
 
         ' Check if the insurance item exists
-        If insurance IsNot Nothing Then
-            lblInsuranceData.Text = insurance.EndDate.ToString()
-        Else
-            lblInsuranceData.Text = "No insurance found"
-        End If
+        lblInsuranceData.Text = "No insurance found"
 
+        ' Change tabs to the details page
         tcTabs.SelectedTab = tpCarDetails
     End Sub
 
+    ' Description:  By pressing this button, the data inserted into the fields in tpAddCar is added into the database
+    ' Parameters:   Default handler parameters
+    ' Return:       NONE
     Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnAddCarEnter.Click
 
         Dim done As Boolean = False
@@ -199,6 +237,9 @@ Public Class formTableViewer
             Case "btnCarDetailsBack"
                 ' Set tab to Cars List
                 tab = tpCarsList
+                lblFuelData.Dispose()
+                lblAvailabilityData.Dispose()
+                lblMilageData.Dispose()
 
             Case "btnShowProblems"
                 ' Set tab to Problems
@@ -221,9 +262,6 @@ Public Class formTableViewer
                 txtName.Clear()
                 txtMileage.Clear()
                 txtAvgFuel.Clear()
-                ' Force garbage collection
-                GC.Collect()
-                GC.WaitForPendingFinalizers()
 
             Case "btnAddCarEnter"
                 ' Set tab to Cars List
@@ -233,6 +271,10 @@ Public Class formTableViewer
                 ' In case something goes wrong, it'll just stay on the same page
                 tab = tcTabs.SelectedTab
         End Select
+
+        ' Force garbage collection
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
 
         ' Change the tab
         tcTabs.SelectedTab = tab
