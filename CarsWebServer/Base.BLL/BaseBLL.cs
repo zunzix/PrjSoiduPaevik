09806af2fb8d6cf5f67fac9b83c1cc8ddb5f449f
ralf <ralf.kaljola@gmail.com>
2025-04-28 22:12:30 +0300

    bll start

---
+++ CarsWebServer/Base.BLL/BaseBLL.cs
@@ -0,0 +1,20 @@
+﻿using Base.BLL.Contracts;
+using Base.DAL.Contracts;
+
+namespace Base.BLL;
+
+public class BaseBLL<TUOW> : IBaseBLL
+    where TUOW : IBaseUOW
+{
+    protected readonly TUOW BLLUOW;
+
+    public BaseBLL(TUOW uow)
+    {
+        BLLUOW = uow;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await BLLUOW.SaveChangesAsync();
+    }
+}
